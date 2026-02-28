b6d41df22c52c47e4aeda798def3eed6570e30cd
Mathieu Marc Pobre <168718907+ReignDer@users.noreply.github.com>
2026-02-28 15:11:24 +0800

    test3

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://reignder.github.io",
-	base: "/Mathieu022826",
+	base: "/my-repo",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
