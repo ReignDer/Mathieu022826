148624a946ed58dacfc2e7b9efff08102939ca8d
Mathieu Marc Pobre <168718907+ReignDer@users.noreply.github.com>
2026-03-14 09:16:09 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Mathieu Marc I. Pobre",
+	title: "Portfolio",
 	subtitle: "Portfolio Site",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
