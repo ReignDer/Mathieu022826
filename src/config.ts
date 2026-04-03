ceb1049c3ab54ab9fa710299653746a8cb908952
Mathieu Marc Pobre <168718907+ReignDer@users.noreply.github.com>
2026-04-03 15:36:39 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/Profile.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Mathieu Marc I. Pobre",
-	bio: "I am an aspiring Game Developer.",
+	bio: "Aspiring Game Developer. Email: mathieu_marc_pobre@dlsu.edu.ph",
 	links: [
 		{
 			name: "Itch-io",
